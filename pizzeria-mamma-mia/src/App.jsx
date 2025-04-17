99eec54f8681e86318222fb205a44e25f19e960a
Citlali Garcia <citlali.garcia.ponce@gmail.com>
2025-04-16 19:09:46 -0600

    se guardan components en carpeta

--- pizzeria-mamma-mia/src/App.jsx
+++ pizzeria-mamma-mia/src/App.jsx
@@ -1,8 +1,8 @@
 
 import './App.css'
-import Navbar from './Navbar'
-import Home from './Home';
-import Footer from './Footer'
+import Navbar from './assets/components/Navbar'
+import Home from './assets/components/Home';
+import Footer from './assets/components/Footer'
 
 
 
