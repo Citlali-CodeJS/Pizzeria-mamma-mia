b51873a5e2d556bdc147bc7cfc40faee394a0db4
Citlali Garcia <citlali.garcia.ponce@gmail.com>
2025-04-29 17:49:51 -0600

    Se creo cart y pizzas

--- pizzeria-mamma-mia/src/App.jsx
+++ pizzeria-mamma-mia/src/App.jsx
@@ -1,23 +1,26 @@
 
 import './App.css'
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+//import Home from './components/Home';
 import Footer from './components/Footer'
+import Cart from './components/Cart';
+// import Register from "./components/Register";
+// import Login from "./components/Login";
 
 
-
-function App() {
+const App = () => {
 
   return (
-    <div>
+    <>
       <Navbar />
-      {/*<Home />*/} 
-      <Register /> 
-     < Login/> 
-    </div>
-  )
-}
+      {/*<Home />*/}
+     {/* <Register />*/}
+     { /*<Login />*/}
+        <Cart/>
+       <Footer />
+    </>
+  );
+};
 export default App;
   
--- pizzeria-mamma-mia/src/components/Home.jsx
+++ pizzeria-mamma-mia/src/components/Home.jsx
@@ -1,40 +1,25 @@
-
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
 import CardPizza from './CardPizza';
-
+import pizzas from './pizzas';
+import { Container, Row, Col } from "react-bootstrap";
 
 const Home = () => {
-    return <>
-    <Header />
-    <div className="container">
-    <div className="row justify-content-center">
-      <div className="col-12 col-md-6 col-lg-4 d-flex justify-content-center mb-4">
-        <CardPizza
-          name="Napolitana"
-          price={5.950}
-          ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-          img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
-        />
-      </div>
-      <div className="col-12 col-md-6 col-lg-4 d-flex justify-content-center mb-4">
-        <CardPizza
-          name="Española"
-          price={6.950}
-          ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-          img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab"
-        />
-      </div>
-      <div className="col-12 col-md-6 col-lg-4 d-flex justify-content-center mb-4">
-        <CardPizza
-          name="Pepperoni"
-          price={6.950}
-          ingredients={["mozzarella", "pepperoni", "orégano"]}
-          img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
-        />
-      </div>
-    </div>
-  </div>
-</>
-}
+  return (
+    <>
+        <Header></Header>
+            <Container className="mt-4">
+                <Row className="justify-content-center">        
+                {pizzas.map((pizza) => (
+                    <Col md={4} className="mb-4 d-flex">
+                        <CardPizza key={pizza.id} pizza={pizza} />
+                    </Col>
+                ))}               
+                </Row>
+            </Container>
+    </>
+  );
+};
+
 
 export default Home;
